6d028be39789f7eb2b1a0b79ae138a3f1bb416bb
DanuAdiP <danu261216@gmail.com>
2019-01-23 21:07:27 +0700

    Commit Pertama

---
+++ 2. LLingkaran.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int r;
+	float phi=3.14,luas;
+	cout<<"MENGHITUNG LUAS LINGKARAN"<<endl<<endl;
+	cout<<"Masukkan jari-jari = ";cin>>r;
+	luas=phi*r*r;
+	cout<<"Hasil              = "<<luas;
+	
+	return 0;
+}
